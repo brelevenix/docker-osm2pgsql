78f894b4198bbfbb59bc5abfb0f22a3484d38661
brelevenix <eric.debeau@gmail.com>
2016-04-29 23:13:58 +0200

    Update end.sh

--- end.sh
+++ end.sh
@@ -4,16 +4,14 @@
 
 #get core
 PROCS=$(grep --count ^processor /proc/cpuinfo)
-echo "NB PROCS: "
 echo $PROCS > /var/lib/start/procs.txt
 
 #get RAM and keep 3/4 for cache
 RAM=$(grep 'MemTotal' /proc/meminfo | perl -pe "s/.*: (.+) .*/\1/")
 CACHE=$(($RAM * 3 / 4000))
-echo "CACHE: "
 echo $CACHE > /var/lib/start/ram.txt
 
-osm2pgsql --create --slim --cache $CACHE --number-processes $PROCS --database gis --username osm --host pg --port 5432 /osm/import.osm.pbf
+osm2pgsql --create --slim --cache $CACHE --number-processes $PROCS --flat-nodes flat-nodes --database gis --username osm --host pg --port 5432 /osm/import.osm.pbf
 
 #command must be executed just once
 touch initialized 
